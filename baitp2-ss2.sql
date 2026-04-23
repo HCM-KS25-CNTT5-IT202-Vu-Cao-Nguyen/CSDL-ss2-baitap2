6eec7bf73dac49e12ce8a1d2a38b64f627a92762
Vu Cao Nguyen <nguyencaovu2007@gmail.com>
2026-04-23 09:30:04 +0700

    sửa lại bài theo yêu cầu đề

--- baitp2-ss2.sql
+++ baitp2-ss2.sql
@@ -1,14 +1,16 @@
-create database baitap2;
-use baitap2;
+CREATE DATABASE baitap2;
+USE baitap2;
 
-drop table if exists customers;
 
-create table customers(
-	customer_id int primary key,
-    fullname varchar(100) not null,
-    email varchar(50) not null,
-    age int not null check (age > 0 and age < 100)
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY
+);
+
+
+ALTER TABLE customers
+ADD fullname VARCHAR(100),
+ADD email VARCHAR(50),
+ADD age INT check (age > 0 and age < 100);
     
     -- đề ban đầu thiếu not null cho cả 3 thuộc tính tránh người dùng để trống
     -- tuổi không thể là số âm nên dùng check để thêm điều kiện
-);
